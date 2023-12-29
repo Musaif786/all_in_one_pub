ac3108aebfc4cc9cd468bcef54094a8375c8bc8b
Musaif786 <mdmusaif.mm@gmail.com>
2023-12-29 13:22:34 +0530

    pushed code through python

--- Python/pullrequest.py
+++ Python/pullrequest.py
@@ -1,25 +1,37 @@
 import subprocess
 import os
+import time
 
 pythonPath = "D:\\Vscode-All scripts\\All_in_one\\Python"
+print(30*"#")
+select = input("Please select the Folder which do you want to update?\n==> for python/terraform press [1]\n==> for other press [2]\n==>").lower()
 
-select = input("Please select the Folder which do you want to update?\n==> for python press [1]\n==> for other press [2]\n==>").lower()
-
-if select == "1" or select == "python":
-    pythonPath
+if select == "1" or select == "python" or select == "terraform":
+    python_Path = pythonPath
+else:
+    print("under process")
 
 
 pullPush = input("Please enter want to pull or push?\n==> for push press [1]\n==> for pull press [2]\n==>").lower()
 
 if pullPush == "push" or pullPush == "1":
-    print("pushing data")
-    subprocess.call(['git','status'] ,cwd=pythonPath)
-    subprocess.call(['git','add','-A'] ,cwd=pythonPath)
-    subprocess.call(['git','commit','-m','pushed code through python'] ,cwd=pythonPath)
-    subprocess.call(['git','push'] ,cwd=pythonPath)
+    try:
+        print("pushing data")
+        subprocess.call(['git','status'] ,cwd=python_Path)
+        subprocess.call(['git','add','-A'] ,cwd=python_Path)
+        subprocess.call(['git','commit','-m','pushed code through python'] ,cwd=python_Path)
+        subprocess.call(['git','push'] ,cwd=python_Path)
+    except NameError as e:
+        print(30*"#")
+        print("Please try again with correct option")
+        print(30*"#")
     
 elif pullPush == "pull" or pullPush == "2":
     print("pulling data")
     subprocess.call(['git','status'] ,cwd=pythonPath)
 else:
+    print(30*"*")
     print("something wet wrong please try again...")
+    print(30*"*")
+
+time.sleep(5)
