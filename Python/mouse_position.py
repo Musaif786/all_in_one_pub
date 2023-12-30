8f53b1bef2128ccc6e39ce2df3ddd60af3751815
Musaif786 <mdmusaif.mm@gmail.com>
2023-12-31 02:05:00 +0530

    pushed code through python

--- Python/mouse_position.py
+++ Python/mouse_position.py
@@ -1,10 +1,12 @@
 import pyautogui as py
-
+import time
 
 try:
     while True:
         position = py.position()
         print(position)
+        time.sleep(0.5)
+        
 except KeyboardInterrupt as k:
     print("Terminated")
 
