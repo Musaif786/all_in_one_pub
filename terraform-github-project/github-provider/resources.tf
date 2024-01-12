85a41670c9bca61fa5d26d910c7d775896a0480a
Musaif786 <mdmusaif.mm@gmail.com>
2024-01-12 13:20:43 +0530

    pushed code through python

--- terraform-github-project/github-provider/resources.tf
+++ terraform-github-project/github-provider/resources.tf
@@ -1,17 +1,17 @@
-resource "github_repository" "terraform-public" {
-  name        = "first-terraform-project"
-  description = "My first-terraform-project to create this repo using teraform auomation for public"
-  visibility = "public"
-  auto_init = true
-  //private = true
-
-}
-
-# resource "github_repository" "terraform-private" {
-#   name        = "second-terraform-project"
-#   description = "My second-terraform-project to create this repo using teraform auomation for private"
-#   visibility = "private"
+# resource "github_repository" "terraform-public" {
+#   name        = "first-terraform-project"
+#   description = "My first-terraform-project to create this repo using teraform auomation for public"
+#   visibility = "public"
 #   auto_init = true
 #   //private = true
 
 # }
+
+resource "github_repository" "terraform-private" {
+  name        = "second-terraform-project"
+  description = "My second-terraform-project to create this repo using teraform auomation for private"
+  visibility = "private"
+  auto_init = true
+  //private = true
+
+}
