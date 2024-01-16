22a28387e3f81ccfdcc59827d6aa3c9c9ff8771a
Musaif786 <mdmusaif.mm@gmail.com>
2024-01-16 23:01:04 +0530

    pushed code through python

--- Python/Dynatrace-api/times.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from datetime import timedelta
-
-
-time = datetime.now()
-print(time)
-#2023-12-07T03:20:00Z
-time_format = time.strftime("%Y-%m-%dT%H:%M:%SZ")
-print(time_format)
