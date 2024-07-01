489675bbafb4732ca0f541394e82dcf825b0ef31
Musaif786 <mdmusaif.mm@gmail.com>
2024-07-01 08:07:13 +0530

    push

---
+++ test.py
@@ -0,0 +1,11 @@
+
+def main():
+    print("hello i'm test file")
+    user = input("enter you name here: ")
+
+    if user =="exit":
+        print("Exiting")
+    else:
+        print(user)
+if __name__ == "__main__":
+    main()
